82420b891a893555251d5c84461791cd1bc93e1c
Maja Crnomarkovic <57286721+crnomarkovicm@users.noreply.github.com>
2020-03-19 22:05:04 +0100

    Dodati opisi igara na karticama koje se okrecu

--- src/lib/home.dart
+++ src/lib/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flip_card/flip_card.dart';
+
 
 class Home extends StatefulWidget {
   @override
@@ -19,7 +21,7 @@ class _HomeState extends State<Home> {
   // imamo mogucnost da izaberemo igru. Ovde pravimo jednu
   // karticu na kojoj ce biti ispisan znak ili ime igre kako
   // bi korisnik lako izabrao koju igru zeli.
-  Widget customcard(naziv_igre, logo_igre){
+  Widget customcard(naziv_igre, logo_igre, opis_igre){
     return Padding(
       padding: EdgeInsets.all(
         20.0,
@@ -29,41 +31,64 @@ class _HomeState extends State<Home> {
           color: Colors.blue,
           elevation: 10.0,
           borderRadius: BorderRadius.circular(20.0),
-          child: Container(
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                  ),
-                  child: Material(
-                    elevation: 5.0,
-                    borderRadius: BorderRadius.circular(100.0),
-                    child: Container(
-                      height: 200.0,
-                      width: 200.0,
-                      child: ClipOval(
-                        child: Image(
-                          image: AssetImage(
-                            logo_igre
+          child: FlipCard(
+            direction: FlipDirection.HORIZONTAL,
+            front: Container(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                      ),
+                      child: Material(
+                        elevation: 5.0,
+                        borderRadius: BorderRadius.circular(100.0),
+                        child: Container(
+                          height: 200.0,
+                          width: 200.0,
+                          child: ClipOval(
+                            child: Image(
+                              image: AssetImage(
+                                  logo_igre
+                              ),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    naziv_igre,
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      color: Colors.white,
+                    Center(
+                      child: Text(
+                        naziv_igre,
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ],
-            )
-          ),
+                  ],
+                )
+            ),
+            back: Container(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        opis_igre,
+                        style: TextStyle(
+                          fontSize: 21.0,
+                          color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+            ),
+          )
         ),
       )
     );
@@ -84,10 +109,10 @@ class _HomeState extends State<Home> {
       ),
       body: ListView(
         children: <Widget>[
-          customcard("Asocijacije", images[0]),
-          customcard("Pogodi jezik", images[1]),
-          customcard("Ko zna zna", images[2]),
-          customcard("Jos jedna igra, smislicemo!", images[3]),
+          customcard("Asocijacije", images[0], "Cilj ove igre je sa sto manje otvorenih polja naci prvo resenja za kolone A, B, C i D a potom i konacno resenje"),
+          customcard("Pogodi jezik", images[1], "Cilj ove igre je otvaranjem datih osobina pogoditi o kom programskom jeziku je rec "),
+          customcard("Ko zna zna", images[2], "Cilj ove igre je odgovoriti na 10 postavljenih pitanja. Za svako od njih ponudjena su 4 odgovora. Ukoliko nakon 10s nije odgovoreno, smatra se da ste pogresili"),
+          customcard("Jos jedna igra, smislicemo!", images[3], "TODO"),
         ],
       )
     );
