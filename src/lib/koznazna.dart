c1e4af3b78eca74ab071f477484ed61e6c84306f
markobabic8 <markobabic056@gmail.com>
2020-04-22 17:01:03 +0200

    Unapredjena igra ko zna zna

--- src/lib/koznazna.dart
+++ src/lib/koznazna.dart
@@ -3,17 +3,16 @@ import 'ui/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'dart:async';
 
 class ko_zna_zna extends StatelessWidget {
-
-
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: DefaultAssetBundle.of(context).loadString("assets/koznazna.json"),
       builder: (context, snapshot){
-        var mydata = json.decode(snapshot.data.toString());
+        List mydata = json.decode(snapshot.data.toString());
         if(mydata == null){
           return Scaffold(
             body: Center(
@@ -27,7 +26,7 @@ class ko_zna_zna extends StatelessWidget {
           );
         }
         else{
-          return koznazna();
+          return koznazna(mydata : mydata);
         }
       },
     );
@@ -35,25 +34,109 @@ class ko_zna_zna extends StatelessWidget {
 }
 
 class koznazna extends StatefulWidget {
+
+  var mydata;
+
+  koznazna({Key key, @required this.mydata}) : super(key : key);
+
   @override
-  _koznaznaState createState() => _koznaznaState();
+  _koznaznaState createState() => _koznaznaState(mydata);
 }
 
 class _koznaznaState extends State<koznazna> {
 
-  Widget choicebutton(){
+  var mydata;
+  _koznaznaState(this.mydata);
+
+  Color colortoshow = Colors.indigoAccent;
+  Color right = Colors.green;
+  Color wrong = Colors.red;
+  int marks = 0;
+  int i = 1;
+  int timer = 10;
+  String showtimer = "10";
+
+  Map<String, Color> btncolor = {
+    "a": Colors.indigoAccent,
+    "b": Colors.indigoAccent,
+    "c": Colors.indigoAccent,
+    "d": Colors.indigoAccent,
+  };
+
+  bool canceltimer = false;
+
+  @override
+  void initState(){
+    starttimer();
+    super.initState();
+  }
+
+  void starttimer() async {
+    const onesec = Duration(seconds: 1);
+    Timer.periodic(onesec, (Timer t){
+      setState(() {
+        if(timer < 1){
+          t.cancel();
+          nextquestion();
+        }
+        else if(canceltimer){
+          t.cancel();
+        }
+        else{
+          timer = timer - 1;
+        }
+        showtimer = timer.toString();
+      });
+    });
+  }
+
+  void nextquestion(){
+    canceltimer = false;
+    timer = 10;
+    setState(() {
+      if(i < 3){
+        i++;
+      }
+      else{
+
+      }
+      btncolor["a"] = Colors.indigoAccent;
+      btncolor["b"] = Colors.indigoAccent;
+      btncolor["c"] = Colors.indigoAccent;
+      btncolor["d"] = Colors.indigoAccent;
+    });
+    starttimer();
+  }
+
+  void checkanswer(String k){
+    if(mydata[2][i.toString()] == mydata[1][i.toString()][k]){
+      marks = marks + 5;
+      colortoshow = right;
+    }
+    else{
+      colortoshow = wrong;
+    }
+    setState(() {
+      btncolor[k] = colortoshow;
+      canceltimer = true;
+    });
+
+    Timer(Duration(seconds: 2), nextquestion);
+  }
+
+  Widget choicebutton(String k){
     return Padding(
       padding: EdgeInsets.symmetric(
         vertical: 10.0,
         horizontal: 20.0,
       ),
       child: MaterialButton(
-          onPressed: (){},
+          onPressed: () => checkanswer(k),
           child: Text(
-            "Odgovor 1",
+            mydata[1][i.toString()][k],
             style: Theme.of(context).textTheme.display2,
           ),
-          color: QuizColors.primary.color,
+          color: btncolor[k],
           splashColor: QuizColors.secondary.color,
           highlightColor: Colors.indigo[700],
           minWidth: 200.0,
@@ -113,7 +196,7 @@ class _koznaznaState extends State<koznazna> {
                 padding: EdgeInsets.all(15.0),
                 alignment: Alignment.bottomLeft,
                 child:  Text(
-                  "Neko pitanje bla bla?",
+                  mydata[0][i.toString()],
                   style: Theme.of(context).textTheme.display1,
                 ),
               ),
@@ -124,10 +207,10 @@ class _koznaznaState extends State<koznazna> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    choicebutton(),
-                    choicebutton(),
-                    choicebutton(),
-                    choicebutton()
+                    choicebutton('a'),
+                    choicebutton('b'),
+                    choicebutton('c'),
+                    choicebutton('d'),
                   ],
                 ),
               ),
@@ -137,7 +220,7 @@ class _koznaznaState extends State<koznazna> {
               child: Container(
                 child: Center(
                   child: Text(
-                      "10",
+                      showtimer,
                       style: Theme.of(context).textTheme.display3
                   ),
                 ),
