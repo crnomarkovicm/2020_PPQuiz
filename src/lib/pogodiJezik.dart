420ddbc384d34f9630668b09d38031a5ea1f030e
Aleksandar Milosevic <aleksandarmilosevic98@gmail.com>
2020-05-02 18:22:49 +0200

    napravljen layout za PogodiJezik

--- src/lib/pogodiJezik.dart
+++ src/lib/pogodiJezik.dart
@@ -51,19 +51,125 @@ class _PogodiJezikState extends State<PogodiJezik> {
   _PogodiJezikState(this.mydata);
 
   Widget Polje(i){
-    return Text(mydata[0]['P'][i.toString()].toString());
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 10.0,
+        horizontal: 20.0,
+      ),
+      child: MaterialButton(
+          onPressed: () {},
+          child: Text(
+            'Ovo je neki tekst',
+            style: Theme
+                .of(context)
+                .textTheme
+                .display2,
+          ),
+          color: Theme.of(context).primaryColor,
+          splashColor: QuizColors.secondary.color,
+          highlightColor: Colors.indigo[700],
+          minWidth: 450.0,
+          height: 60.0,
+          shape: BeveledRectangleBorder(
+            side: BorderSide(
+              color: Colors.indigo[700]
+            )
+          )
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        Polje(1),
-        Polje(2),
-        Polje(3),
-        Polje(4),
-        Polje(5)
-      ],
+    return WillPopScope(
+      onWillPop: (){
+        return showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text(
+                "PPQuiz",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              content: Text(
+                "Da li ste sigurni da zelite da izadjete iz aplikacije dok je igra 'Pogodi jezik' u toku?",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  onPressed: (){
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    'Ne',
+                  ),
+                ),
+                FlatButton(
+
+                  child: Text(
+                    'Da',
+                  ),
+                  onPressed: () {
+                    exit(0);
+                  },
+                ),
+              ],
+            )
+        );
+      },
+      child: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Flexible(
+              child: Container(
+                width: 100,
+                height: 80,
+              ),
+              flex: 2,
+            ),
+            Center(
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Polje(1),
+                    Polje(2),
+                    Polje(3),
+                    Polje(4),
+                    Polje(5)
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: Container(
+                margin: EdgeInsets.only(
+                  top: 60
+                ),
+                width: 300,
+                height: 80,
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.body1,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.indigo[700],
+                        width: 2
+                      )
+                    )
+                  ),
+                ),
+              )
+            )
+
+          ],
+        ),
+      ),
     );
   }
 }
